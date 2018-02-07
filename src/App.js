6547077ce1532a0e009f08f7a4c07e85b3943b6b
James Jenkins <jamesjenkinsjr@icloud.com>
2018-02-06 19:47:47 -0500

    Made the increment and decrement buttons work on counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      count: 0
+    }
+  }
   render() {
     return (
-      <div>
-        <h1>Hello</h1>
-        <h2>Something else</h2>
-      </div>
+     <div>
+       <button onClick={() => this.setState({count: this.state.count + 1})}>+</button>
+       <button onClick={() => this.setState({count: this.state.count - 1})}>-</button>
+       <h1>{this.state.count}</h1>
+     </div>
     );
   }
 }
