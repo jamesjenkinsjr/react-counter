b453d05af05b4ad8b7115efc1f0fc0ec4ecd49aa
James Jenkins <jamesjenkinsjr@icloud.com>
2018-02-06 21:11:00 -0500

    Allow for custom increment and decrement values

--- src/App.js
+++ src/App.js
@@ -4,23 +4,30 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      count: 0
+      count: 0,
+      value: 0
     }
     this.increment = this.increment.bind(this); //Binding means, inside of increment function, the this keyword is the component
     this.decrement = this.decrement.bind(this);
+    this.valueChange = this.valueChange.bind(this);
   }
   increment(){
-    this.setState({count: this.state.count + 1});
+    this.setState({count: this.state.count + this.state.value});
   }
   decrement(){
-    this.setState({count: this.state.count - 1});
+    this.setState({count: this.state.count - this.state.value});
+  }
+  valueChange(e){
+    this.setState({value: +e.target.value});
   }
   render() {
     return (
      <div>
+       <input type="number" onChange={this.valueChange} value={this.state.value}/>
        <button onClick={this.increment}>+</button>
        <button onClick={this.decrement}>-</button>
        <h1>{this.state.count}</h1>
+       <button onClick={() => this.setState({count: 0})}>RESET MUTHALUVA</button>
      </div>
     );
   }
