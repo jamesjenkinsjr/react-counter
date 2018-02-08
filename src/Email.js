5d85590f024defe4a1f827c915db63dd7286e522
James Jenkins <jamesjenkinsjr@icloud.com>
2018-02-07 20:23:26 -0500

    Add p and place in jumbotron

---
+++ src/Email.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Email extends Component {
+    render(){
+        return(
+            <div className="jumbotron">
+                <h1>EMAIL</h1>
+                <p>blah@blah.com</p>
+            </div>
+        );
+    }
+}
+
+export default Email;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker'; 
+//import defaults to looking in node_modules
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
