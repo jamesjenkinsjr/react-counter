ac4a1d3cc04b9b4987ffdfb22554485c0427a1bb
James Jenkins <jamesjenkinsjr@icloud.com>
2018-02-07 19:56:19 -0500

    Installed and applied bootstrap classes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
     super();
     this.state = {
       count: 0,
-      value: 0
+      value: ''
     }
     this.increment = this.increment.bind(this); //Binding means, inside of increment function, the this keyword is the component
     this.decrement = this.decrement.bind(this);
@@ -23,11 +23,13 @@ class App extends Component {
   render() {
     return (
      <div>
-       <input type="number" onChange={this.valueChange} value={this.state.value}/>
-       <button onClick={this.increment}>+</button>
-       <button onClick={this.decrement}>-</button>
+       <div className="form-group">
+        <input className="form-control" type="number" onChange={this.valueChange} value={this.state.value}/>
+      </div>
+       <button className="btn btn-large btn-primary" onClick={this.increment}>+</button>
+       <button className="btn btn-large btn-primary" onClick={this.decrement}>-</button>
        <h1>{this.state.count}</h1>
-       <button onClick={() => this.setState({count: 0})}>RESET MUTHALUVA</button>
+       <button className="btn btn-large btn-primary" onClick={() => this.setState({count: 0})}>RESET MUTHALUVA</button>
      </div>
     );
   }
